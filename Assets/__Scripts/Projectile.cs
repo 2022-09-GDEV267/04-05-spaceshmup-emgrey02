bf62d0b2e518cc9b92b4a8d1adb018357ed9a1c2
Emma Grey <emgrey02@gmail.com>
2022-10-03 16:52:43 -0400

    Make projectile destroy enemies

--- Assets/__Scripts/Projectile.cs
+++ Assets/__Scripts/Projectile.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-
+    private BoundsCheck bndCheck;
    
     private void Awake()
     {
-
+        bndCheck = GetComponent<BoundsCheck>();
     }
 
-    private void Update()
+    void Update()
     {
-      
+        if (bndCheck.offUp)
+        {
+            Destroy(gameObject);
+        }
     }
 
     
