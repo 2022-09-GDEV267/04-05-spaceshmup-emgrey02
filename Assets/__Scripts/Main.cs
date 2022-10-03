757dba653685d364a1afab968a0a6016cce2b75d
Emma Grey <emgrey02@gmail.com>
2022-10-03 15:51:19 -0400

    Spawn enemies

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
-
-
+    static public Main S;
 
     [Header("Set in Inspector")]
-    bool placeholder2; // here to keep VS from freaking out - DELETE IT
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyDefaultPadding = 1.5f;
 
-
-
-    
+    private BoundsCheck bndCheck;
 
     private void Awake()
     {
-        
+        S = this;
+        bndCheck = GetComponent<BoundsCheck>();
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 
     public void SpawnEnemy()
     {
-        
+        // pick random enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        //position enemy above screen w/ random x position
+        float enemyPadding = enemyDefaultPadding;
+        if (go.GetComponent<BoundsCheck>() != null)
+        {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        //set initial position for spawned enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 
     public void DelayedRestart(float delay)
